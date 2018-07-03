a4c954d8ba60a9a3f9e3d15bbfa0b0da7de875e7
Connie Leung <cleung334@gmail.com>
2018-07-04 07:29:22 +0800

    Move the arrays to separate file

---
+++ src/app/data.ts
@@ -0,0 +1,114 @@
+// ARRAY 1
+export const texasss = [
+	{
+		name: 'Mike',
+		age: 23,
+		gender: 'm',
+		us: false
+	},
+	{
+		name: 'Liz',
+		age: 20,
+		gender: 'f',
+		us: true
+	},
+	{
+		name: 'Chris',
+		age: 102,
+		gender: 'm',
+		us: true
+	},
+	{
+		name: 'Chuloo',
+		age: 27,
+		gender: 'm',
+		us: false
+	},
+	{
+		name: 'Annie',
+		age: 30,
+		gender: 'f',
+		us: true
+	}
+];
+
+// Part 1 - Find all users older than 24
+// Part 2 - Find the total age of all users
+// Part 3 - List all female coders
+
+// ARRAY 2
+export const newieyork = [
+	{
+		name: 'Michelle',
+		age: 65,
+		coder: true,
+		gender: 'f',
+		us: true
+	},
+	{
+		name: 'Sam',
+		age: 25,
+		coder: false,
+		gender: 'm',
+		us: false
+	},
+	{
+		name: 'Ivy',
+		age: 26,
+		coder: true,
+		gender: 'f',
+		us: false
+	},
+	{
+		name: 'Nick',
+		age: 32,
+		coder: true,
+		gender: 'm',
+		us: true
+	},
+	{
+		name: 'Jim Beglin',
+		age: 17,
+		coder: false,
+		gender: 'm',
+		us: true
+	}
+];
+
+// Part 1 - List all users in US in ascending order
+// Part 2 - Sort all users by age
+// Part 3 -  List all female coders
+
+// ARRAY 3
+export const vegzas = [
+	{
+		name: 'Charly',
+		age: 22,
+		coder: true,
+		gender: 'm'
+	},
+	{
+		name: 'Law',
+		age: 21,
+		coder: true,
+		gender: 'm'
+	},
+	{
+		name: 'Rosey',
+		age: 42,
+		coder: false,
+		gender: 'f'
+	},
+	{
+		name: 'Steph',
+		age: 18,
+		coder: true,
+		gender: 'f'
+	},
+	{
+		name: 'Jon',
+		age: 47,
+		coder: false,
+		gender: 'm'
+	}
+];
